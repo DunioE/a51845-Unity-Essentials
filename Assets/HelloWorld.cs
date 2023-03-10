11436d71931918d7e90fa83c782fd9dc81b4ad73
DunioE <126677297+DunioE@users.noreply.github.com>
2023-03-10 17:34:33 +0000

    5. Adicione uma propriedade com uma variável

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
+
 {
+    public string myMessage;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class HelloWorld : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hello World");
+        Debug.Log(myMessage);
     }
 }
